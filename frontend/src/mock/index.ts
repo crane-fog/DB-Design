43427e4c8ea2c5992d1e191d47b6a60a5ddf5e71
AmThe404 <2074952711@qq.com>
2026-07-29 21:29:56 +0800

    feat(mock): add versioned persistent storage

---
+++ frontend/src/mock/index.ts
@@ -0,0 +1,10 @@
+export {
+  createMockStore,
+  MOCK_DATA_KEY,
+  MOCK_SCHEMA_VERSION,
+  MOCK_STORAGE_NAMESPACE,
+  mockDelay,
+  nextMockId,
+  resetMockData,
+} from './storage'
+export type { MockStore } from './storage'
